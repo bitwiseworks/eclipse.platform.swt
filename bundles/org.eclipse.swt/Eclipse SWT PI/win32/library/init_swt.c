08168a60e06d88cead38e027400ebab8cfa73059
Ben Rietbroek <rousseau.ecsdev@gmail.com>
2015-05-30 06:20:02 +0800

    Update Build Level

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/init_swt.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/init_swt.c
@@ -20,7 +20,7 @@
 #ifndef BUILD_MACHINE
 #define BUILD_MACHINE "**UNKNOWN**"
 #endif
-static const char bldlevel[] = "@#Project swt-os2:0.5#@##1## 08 May 2015 00:00:00     "BUILD_MACHINE"::::5::@@Standard Widget Toolkit for OS/2 (alpha-1)";
+static const char bldlevel[] = "@#Project swt-os2:0.5#@##1## 29 May 2015 00:00:00     "BUILD_MACHINE"::::7::@@Standard Widget Toolkit for OS/2 (alpha-1+)";
 
 /* Prototypes */
 BOOL WINAPI DllMain(HANDLE hInstDLL, DWORD dwReason, LPVOID lpvReserved);
